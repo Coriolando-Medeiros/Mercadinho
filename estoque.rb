224327996243734490a274f06094f3fa4b7d90db
Coriolando <coriolando.medeiros@gmail.com>
2024-09-07 21:46:55 -0300

    feat: adicionar as funcionalidades relacionadas ao estoque

---
+++ estoque.rb
@@ -0,0 +1,40 @@
+require_relative 'produto'
+
+class Estoque
+  def initialize
+    @entregas = []
+    
+  end
+
+  def adicionar
+    puts "Adicionar produto"
+    print "Fornecedor: "
+    fornencedor = gets.chomp.capitalize
+    print "Entregador: "
+    entregador = gets.chomp.capitalize
+    print "Vendedor: "
+    vendedor = gets.chomp.capitalize
+    print "Quantidade de itens da entrega: "
+    itens = gets.chomp.to_i
+    hora = Time.now.strftime("%H:%M:%S")
+    data = Time.now.strftime("%d-%m-%Y")
+    contador = 0
+    produto = Produto.new
+    until contador == itens
+      produto.cadastrar_produto
+      contador += 1
+    end
+    @entregas << {fornencedor: fornencedor, entregador: entregador, vendedor: vendedor, data: data}
+    File.open("entregas.txt", "a") do |arquivo|
+      arquivo.puts "Fornecedor: #{fornencedor} | Entregador: #{entregador} | Vendedor: #{vendedor} | Data: #{data} | Hora: #{hora}"
+    end
+    puts "Atualizado estoque #{data} #{hora}"
+  end
+
+  def saidas
+    
+  end
+end
+
+estoque = Estoque.new
+estoque.adicionar
