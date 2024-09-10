f87f4c736d0f53d38c292387eceb54b92d5085c2
Coriolando <coriolando.medeiros@gmail.com>
2024-09-10 06:07:14 -0300

    feat(admin): adicionar menu geral de administrador

--- admin.rb
+++ admin.rb
@@ -1,18 +1,37 @@
 class Administrador
-  require_relative 'vendas'
-
-  def initialize
-    @vendas = Vendas.new('estoque.txt')
-  end
 
   def ver_relatorios
     @vendas.historico_vendas
   end
 
-  def atualizar_estoque
-    
+  def menu_admin_estoque
+    loop do
+      puts "Menu"
+      puts "1 - Fornecdor"
+      puts "2 - Produto"
+      puts "3 - Históricos"
+      puts "0 - Sair"
+      print "Opção: "
+      opcao = gets.chomp
+
+      if opcao == '1'
+        menu_fornecedor
+      elsif opcao == '2'
+        menu_produto
+      elsif opcao == '3'
+        menu_estoque
+      elsif opcao == '0'
+        puts "Saindo..."
+        return
+      else
+        puts "Opção inválida! Tente novamente"
+      end
+    end
   end
+
+  
 end
 
+
 admin = Administrador.new
-admin.ver_relatorios
+admin.menu_admin_estoque
